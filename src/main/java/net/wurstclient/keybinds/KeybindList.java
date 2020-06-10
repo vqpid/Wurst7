2c4cf55adea5def4e030f143c58ec97c7146c5c7
vqpid <66125069+vqpid@users.noreply.github.com>
2020-06-10 13:52:11 +0200

    Update KeybindList.java

--- src/main/java/net/wurstclient/keybinds/KeybindList.java
+++ src/main/java/net/wurstclient/keybinds/KeybindList.java
@@ -119,11 +119,13 @@ public final class KeybindList
 		addKB(set, "b", "fastplace;fastbreak");
 		addKB(set, "b", "fastplace;fastbreak");
 		addKB(set, "c", "fullbright");
+		addKB(set, "d", "dupeeSP");
 		addKB(set, "g", "flight");
 		addKB(set, "semicolon", "speednuker");
 		addKB(set, "h", "say /home");
 		addKB(set, "j", "jesus");
 		addKB(set, "k", "multiaura");
+		addKB(set, "m", "mountbypass");
 		addKB(set, "n", "nuker");
 		addKB(set, "r", "killaura");
 		addKB(set, "right.shift", "navigator");
